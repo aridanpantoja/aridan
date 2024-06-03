5717a169cb7e331eedc5bf202b804a52b5da84c5
Aridan Pantoja <70522543+aridanpantoja@users.noreply.github.com>
2024-06-03 11:42:58 +0000

    fix: change social links

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -1,7 +1,6 @@
 import { LucideIcon } from 'lucide-react'
 import { IconType } from 'react-icons'
 import { FaCodeBranch, FaGithub, FaLinkedin } from 'react-icons/fa'
-import { FaRegNewspaper } from 'react-icons/fa6'
 
 interface LinkItemProps {
   label: string
@@ -20,11 +19,6 @@ export const pages: LinkItemProps[] = [
     Icon: FaCodeBranch,
     href: '/projetos',
   },
-  {
-    label: 'Blog',
-    Icon: FaRegNewspaper,
-    href: '/blog',
-  },
 ]
 
 export const social: LinkItemProps[] = [
@@ -36,7 +30,7 @@ export const social: LinkItemProps[] = [
   {
     label: 'Linkedin',
     Icon: FaLinkedin,
-    href: 'https://www.linkedin.com/in/aridan-pantoja',
+    href: 'https://www.linkedin.com/in/aridanpantoja',
   },
 ]
 
